fd2db17406716a2e8623dd366c3f6cb041e6623b
Aaron-Samuel05 <157136322+Aaron-Samuel05@users.noreply.github.com>
2026-09-23 17:26:34 +0530

    Fix remaining JSX newline syntax

--- app/components/IPhone3D.tsx
+++ app/components/IPhone3D.tsx
@@ -130,7 +130,8 @@ export default function IPhone3D(){
     <directionalLight position={[-5,2,3]} intensity={1.4}/>
     <Environment preset="studio" environmentIntensity={.7}/>
     <PhoneModel/>
-    <ContactShadows position={[0,-3.15,0]} opacity={.24} scale={5.8} blur={2.6} far={4.5}/>\n    <OrbitControls enabled={!isMobile} enablePan={false} enableZoom={false} enableDamping dampingFactor={0.08} rotateSpeed={0.65} minPolarAngle={Math.PI*.36} maxPolarAngle={Math.PI*.64} minAzimuthAngle={-Math.PI*.45} maxAzimuthAngle={Math.PI*.45} />
+    <ContactShadows position={[0,-3.15,0]} opacity={.24} scale={5.8} blur={2.6} far={4.5}/>
+    <OrbitControls enabled={!isMobile} enablePan={false} enableZoom={false} enableDamping dampingFactor={0.08} rotateSpeed={0.65} minPolarAngle={Math.PI*.36} maxPolarAngle={Math.PI*.64} minAzimuthAngle={-Math.PI*.45} maxAzimuthAngle={Math.PI*.45} />
   </Canvas>;
 }
 
